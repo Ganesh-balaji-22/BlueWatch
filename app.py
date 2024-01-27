c7a710cfa1b8b30946b6541f0c9596f15137e370
Ganesh-balaji-22 <145870375+Ganesh-balaji-22@users.noreply.github.com>
2024-01-27 23:02:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+
+import streamlit as st
+from PIL import Image
+import cv2
+import os
+import shutil 
+from ultralytics import YOLO
+from moviepy.editor import VideoFileClip
+
+def avi_mp(avi_file, mp4_file):
+
+    video_clip = VideoFileClip(avi_file)
+    video_clip.write_videofile(mp4_file, codec="libx264", audio_codec="aac")
+    video_clip.close()
+
+def yolov8(img_path):
+    
+    model=YOLO("Model/best.pt")
+    result=model.predict(img_path,save=True)
+    return result
+
+def main():
+    st.title("Media Uploader")
+
+    media_type = st.radio("Select media type", ["Image", "Video"])
+
+    uploaded_file = st.file_uploader(f"Choose a {media_type.lower()} file", type=["jpg", "jpeg", "png", "mp4"])
+    
+    
+    if uploaded_file is not None:
+        file_name=uploaded_file.name.split('.')[0]
+        file_extension = uploaded_file.name.split('.')[-1]
+
+        # Save the uploaded file to the input folder
+        input_folder = "input"
+        os.makedirs(input_folder, exist_ok=True)
+        file_path = os.path.join(input_folder, uploaded_file.name)
+
+        with open(file_path, "wb") as file:
+            file.write(uploaded_file.read())
+
+        st.write(f"{media_type} saved to {file_path}")
+        x=yolov8(file_path)
+        output_path="runs/detect/predict/"+file_name+".avi"
+
+        if media_type == "Image" and file_extension in ["jpg", "jpeg", "png"]:
+            # Display the uploaded image
+            image = Image.open(uploaded_file)
+            st.image(image, caption="Uploaded Picture", use_column_width=True)
+            st.write("")
+            st.write("Classifying image... (add your classification logic here)")
+
+        elif media_type == "Video" and file_extension == "mp4":
+            # Display the uploaded video
+            st.video(uploaded_file)
+            st.write("")
+            st.write("Analyzing video... (add your analysis logic here)")
+        mp4_path="output/1.mp4"
+        avi_mp(output_path,mp4_path)
+        st.video(mp4_path)
+        shutil.rmtree('runs/detect')
+main()
